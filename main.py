af9133e78af06b87fe9e2e1495c310621e913cf8
Ebesko <118739260+Ebesko@users.noreply.github.com>
2023-10-17 00:30:16 +0200

    Working score
    To do :
    - fix the center screen
    - arrange in different files

--- main.py
+++ main.py
@@ -15,8 +15,16 @@ class FailureManagement:
         # Checkboxes for all
         self.Chk_6 = tk.IntVar()
 
-        # Set minimum window size (width, height)
-        self.root.minsize(500, 500)  # Example minimum size
+        # Scale attribute
+        self.scaleforce = tk.DoubleVar()
+
+        # Radiobutton attribute
+        self.radio_value = tk.IntVar()
+
+        # Set minimum window size (width, height) and start position of the window to the center
+        self.root.geometry("500x500+700+300")
+        # Block the resize
+        self.root.resizable(False, False)
 
         # Create a top frame for the title
         self.create_top_frame()
@@ -58,10 +66,9 @@ class FailureManagement:
 
     def create_low_self_esteem(self, frame):
         """For low choice radiobutton"""
-        value = tk.IntVar()
-        button1 = tk.Radiobutton(frame, text="Ok", variable=value, value=1)
-        button2 = tk.Radiobutton(frame, text="Terrible", variable=value, value=2)
-        button3 = tk.Radiobutton(frame, text="Awful & I want to die", variable=value, value=3)
+        button1 = tk.Radiobutton(frame, text="Ok", variable=self.radio_value, value=1)
+        button2 = tk.Radiobutton(frame, text="Terrible", variable=self.radio_value, value=2)
+        button3 = tk.Radiobutton(frame, text="Awful & I want to die", variable=self.radio_value, value=3)
         button1.pack(side=tk.TOP, padx=10, pady=5, anchor="w")
         button2.pack(side=tk.TOP, padx=10, pady=5, anchor="w")
         button3.pack(side=tk.TOP, padx=10, pady=5, anchor="w")
@@ -104,24 +111,77 @@ class FailureManagement:
     def create_despair(self, frame):
         """Scale of the Loserness"""
         # Create a scale from 0 to 100
-        value = tk.DoubleVar()
-        scale_0_to_100 = tk.Scale(frame, variable=value, from_=100, to=0)
+        scale_0_to_100 = tk.Scale(frame, variable=self.scaleforce, from_=100, to=0)
         scale_0_to_100.pack()
 
     def screwing(self):
-        """Score the Luser"""
-        loserness = 0
+        """Score the Loser"""
+        # adds scale, chkboxes, radiobutton to get a score
+        loserness = self.scaleforce.get() \
+                    + self.Chk_1.get() \
+                    + self.Chk_2.get() \
+                    + self.Chk_3.get() \
+                    + self.Chk_4.get() \
+                    + self.Chk_5.get() \
+                    + self.Chk_6.get() \
+                    + self.radio_value.get()
 
+        # Setup of the results window
+        result_window = tk.Tk()
+        result_window.title("Inefficiancy results :")
+        result_window.geometry("300x200+800+400")
+        result_window.minsize(300, 200)
+        result_window.resizable(False, False)
+
+        # print the results
+        # itself_the_result = tk.Label(result_window, text="BRUH", font=("Helvetica", 12))
+        # itself_the_result.grid(column=2, row=2, sticky="nsew")
+        # itself_the_result.pack(expand=True, fill="both")
+
+        if loserness <= 1:
+            # print(loserness)
+            lies_label = tk.Label(result_window, text="FATAL ERROR : \n\nU LYING", font=("Helvetica", 12))
+            lies_label.pack(expand=True, fill="both")
+            # print("FATAL ERROR : U LYING")
+        if loserness == 109:
+            # print(loserness)
+            best_loser = tk.Label(result_window, text="THE CHOOSEN LOSER ARRIVED ! ", font=("Helvetica", 12))
+            best_loser.pack(expand=True, fill="both")
+            # print("THE CHOOSEN LOSER ARRIVED ! ")
+        if 55 > loserness > 1:
+            # print(loserness)
+            true_loser = tk.Label(result_window, text="You have a good opinion of yourself !... \n\nYou shouldn't. "
+                                                      "Really.",
+                                  font=("Helvetica", 12))
+            true_loser.pack(expand=True, fill="both")
+            # print("You have a good opinion of yourself !... You shouldn't. Really.")
+        if 109 > loserness >= 55:
+            # print(loserness)
+            mild_loser = tk.Label(result_window,
+                                  text="Mild loser. Bad at most of the things, \n\nstill think he will be good at "
+                                       "something...", font=("Helvetica", 12))
+            mild_loser.pack(expand=True, fill="both")
+            # print("Mild loser. Bad at most of the things, still think he will be good at something...")
 
     def more_buttons(self, frame):
         """Button of screwing and options to escape"""
-        # To screw the user
+        # To screw with the user
         button_score = tk.Button(frame, text="Score", font=("Helvetica", 11), command=lambda: self.screwing())
         button_score.pack(side=tk.TOP, padx=10, pady=5)
 
-        # Button More Options (MOVE IT)
-        button_options = tk.Button(frame, text="Options", font=("Helvetica", 11))
+        # Button More Options
+        button_options = tk.Button(frame, text="Option", font=("Helvetica", 11), command=lambda: self.no_escape())
         button_options.pack(side=tk.TOP, padx=10, pady=5)
+
+    def no_escape(self):
+        not_escaped = tk.Tk()
+        not_escaped.title("You messed up again.")
+        not_escaped.geometry("300x200+800+400")
+        not_escaped.minsize(300, 200)
+        not_escaped.resizable(False, False)
+        stay_here = tk.Label(not_escaped, text="THERE IS NO ESCAPE!", font=("Helvetica", 12))
+        stay_here.pack(expand=True, fill="both")
+
 
     def meet_your_end(self, frame):
         """Really just a close button"""
